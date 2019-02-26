998a1dda849507bc08a3de11a629da63cc6916e6
lengxiaohua <lengxiaohua1024@gmail.com>
2019-02-26 16:43:38 +0800

    Vue"

---
+++ vue/vueDemo/src-base/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img class="logo" src="./assets/logo.png" alt="Logo" />
+
+    <!-- 3.使用组件标签 -->
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+//1.引入组件
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  //映射组件标签
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+.logo {
+  width: 200px;
+  height: 200px;
+}
+</style>
---
+++ vue/vueDemo/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+        <Add :addComment="addComment"/>
+        <List :comments="comments" :deleteComment="deleteComment"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Add from './components/Add.vue'
+import List from './components/List.vue'
+export default {
+
+   data () {
+      return {
+        comments: [   //数据在哪个组件，更新数据的行为（方法）就应该定义在哪个组件
+          {
+            name : 'BOB',
+            content: 'Vue 还不错'
+          },
+          {
+            name: 'Cat',
+            content: 'Vue So Easy'
+          },
+          {
+            name: 'BZ',
+            content: 'Vue So So'
+          }
+        ]
+      }
+    },
+
+    methods: {
+      //添加评论
+      addComment(comment) {
+        this.comments.unshift(comment)
+      },
+
+      //删除指定下标的评论
+      deleteComment (index) {
+        this.comments.splice(index, 1)
+      }
+    },
+
+    components: {
+        Add,
+        List
+    }
+};
+</script>
+
+<style>
+
+</style>
