9f9cb6df24474e2d46f086db22e1b564850e1e2c
lengxiaohua <lengxiaohua1024@gmail.com>
2019-03-08 13:44:51 +0800

    algorithm-算法

--- algorithm-算法/SortTestHelper/UtilMethod.js
+++ algorithm-算法/SortTestHelper/UtilMethod.js
@@ -1,12 +1,20 @@
 (function (window) {
     var MyUtilMethod = {
         //生成有n个元素的随机数组，每个元素的随机范围为[rangeL,rangeR]
-        "generateRandomArray" : function(n, rangeL, rangeR){
+        "generateRandomArray": function (n, rangeL, rangeR) {
             var arr = [];
-            for(var i=0; i<n; i++){
-                arr[i] = Math.round(Math.random()*(rangeR - rangeL + 1) + rangeL);
+            for (var i = 0; i < n; i++) {
+                arr[i] = Math.round(Math.random() * (rangeR - rangeL + 1) + rangeL);
             }
             return arr;
+        },
+        //打印数组的函数
+        "printArray": function (idName,arrName) {
+            var arrStr = '';
+            for (var i = 0; i < n; i++) {
+                arrStr += (`<li>${arrName[i]}</li>`);
+            }
+            $('#' + idName).append(arrStr);
         }
     };
     window.MyUtilMethod = MyUtilMethod;
