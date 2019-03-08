f0edaf705dcc75f4d652c689fa55c2135c56d488
lengxiaohua <lengxiaohua1024@gmail.com>
2019-03-08 14:50:18 +0800

    algorithm-算法

--- algorithm-算法/SortTestHelper/UtilMethod.js
+++ algorithm-算法/SortTestHelper/UtilMethod.js
@@ -16,7 +16,28 @@
                 arrStr += (`<li>${arr[i]}</li>`);
             }
             $('#' + idName).append(arrStr);
+        },
+        //判断数组是否是已经排序的
+        "isSorted" : function (arr) {
+            var n = arr.length;
+            for(var i=0; i < n-1; i++){
+                if(arr[i] > arr[i+1]){
+                    return false;
+                }
+            }
+            return true;
+        },
+        //测试代码性能的函数
+        "testSort" : function(methodName, methodFun, arr, showDivId){
+            var start = new Date().getTime();
+            methodFun(arr);
+            var end = new Date().getTime();
+            if(this.isSorted(arr)){
+                $('#' + showDivId).append(`${methodName} -->${end - start}`);
+            } else {
+                $('#' + showDivId).append(`fail to Sort`);
+            }
         }
-    };
+     };
     window.MyUtilMethod = MyUtilMethod;
 })(window);
